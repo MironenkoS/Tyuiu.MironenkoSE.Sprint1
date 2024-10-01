d3fcff3d29171be6f3050c15d6bd0376fb389d03
664 <stephencurrytopone@gmail.com>
2024-10-01 11:58:35 +0500

    Task 0

---
+++ Tyuiu.MironenkoSE.Sprint1.Task0.V6.Test/DataServiceTEst.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.MironenkoSE.Sprint1.Task0.V6.Lib;
+namespace Tyuiu.MironenkoSE.Sprint1.Task0.V6.Test
+{
+    [TestClass]
+    public class DataServiceTEst
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            var res = ds.Calculate();
+            Assert.AreEqual(15,res);
+        }
+    }
+}
