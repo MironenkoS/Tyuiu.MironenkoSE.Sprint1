79d624d2e89967fe76798ad049743c6616582bfe
664 <stephencurrytopone@gmail.com>
2024-12-27 15:45:59 +0500

    Task 5

---
+++ Tyuiu.MironenkoSE.Sprint1.Task5.V1.Test/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace Tyuiu.MironenkoSE.Sprint1.Task5.V1.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
