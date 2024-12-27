f5f6e0e9e9103c0dd2824145b740f4f23f30a0d9
664 <stephencurrytopone@gmail.com>
2024-12-27 16:09:41 +0500

    Task 6

---
+++ Tyuiu.MironenkoSE.Sprint1.Task6.V9/Program.cs
@@ -0,0 +1,11 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint1;
+namespace Tyuiu.MironenkoSE.Sprint1.Task6.V9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
